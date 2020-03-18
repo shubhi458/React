71b6f915f17601095c93b79d9a8b580898f7269a
Shubhi Saxena <saksenashubhi14@gmail.com>
2020-03-18 16:28:23 +0530

    comment

--- src/Form.js
+++ src/Form.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
+import { withRouter } from 'react-router-dom'
 
-import {withRouter} from 'react-router-dom'
 class Form extends Component{
     obj=[];
     constructor(props){
@@ -8,7 +8,67 @@ class Form extends Component{
         this.state={
             username:"",
             skills:"",
-            technology:""
-        }
+            technology:"",
+            email:""
+        };
     }
+
+    componentDidMount(){
+        this.reactRef.current.focus();
+    }
+    Handler(event){
+        this.setState({
+            [event.target.name]:event.target.value;
+        });
+    }
+
+    formHandler(){
+        this.obj=[];
+        this.storage=JSON.parse(localStorage.getItem("formData"));
+        if(this.storage)
+        {
+            this.obj=this.storage;
+        }
+        this.obj=[...this.obj,this.state];
+        localStorage.setItem("formdata",JSON.stringify(this.obj));
+        this.props.history.push("/home");
+    }
+
+    render(){
+        return (
+            <Container>
+                <Row>
+                <h2>Login Form</h2>
+                </Row>
+            <Row>
+            <Col sm="12" md={{ size: 6, offset: 3 }}>
+            <Form inline ref="form">
+            <FormGroup>
+                <Label for="exampleEmail" hidden>User Name</Label>
+                <Input type="text" name="uname" id="exampleEmail" placeholder="Username"/>
+              </FormGroup>
+              {' '}
+              <FormGroup>
+                <Label for="exampleEmail" hidden>Skills</Label>
+                <Input type="text" name="skills" id="exampleEmail" placeholder="Skills"/>
+              </FormGroup>
+              {' '}
+              <FormGroup>
+                <Label for="exampleEmail" hidden>Technology</Label>
+                <Input type="text" name="technology" id="exampleEmail" placeholder="Technology"/>
+              </FormGroup>
+              {' '}
+              <FormGroup>
+                <Label for="exampleEmail" hidden>Email</Label>
+                <Input type="email" name="email" id="exampleEmail" placeholder="Email"/>
+              </FormGroup>
+              {' '}
+            
+              <Button onClick={() => this.onSubmit()}>Submit</Button>
+            </Form>
+            </Col>
+            </Row>
+            </Container>
+          );
+       } 
 }
